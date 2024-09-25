a353bf1aeb414bf0763906be15af1d250b714794
[genie-ru] <[genie.zgmf987@gmail.com]>
2024-09-25 23:28:12 +0900

    test

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	
+	var (
+		i int = 1
+		s string = "hello"
+		f64 float64 = 1.0
+		t, f bool = true, false
+	)
+	fmt.Println(i, s, f64, t, f)
+
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
 }
